97aa5e7c79d764991da851149a78acd2067b7e10
kate.lavrinovich <kate.lavrinovich>
2016-09-07 12:23:29 +0300

    task jd01

---
+++ src/by/it/laurynovich/jd01_03/InOut.java
@@ -0,0 +1,42 @@
+package by.it.laurynovich.jd01_03;
+
+import java.util.Scanner;
+
+/**
+ * Created by vseotdala on 02.09.2016.
+ */
+public class InOut {
+    private static double[] mas;
+    private static String name;
+    private static int cols;
+
+    public static String consoleReadLine(){
+        return new Scanner(System.in ).nextLine();
+    }
+
+    static double[] arrayFromString(String str){
+        str=str.trim();
+        String strArray[]=str.split(" ");
+        double res[]=new double[strArray.length];
+        for (int i = 0; i < strArray.length; i++) {
+            res[i]=Double.parseDouble( strArray[i] );
+
+        }
+        return res;
+
+    }
+    static void arrayPrint (double mas[],String name, int cols){
+        InOut.mas = mas;
+        InOut.name = name;
+        InOut.cols = cols;
+        int col=0;
+        for (double value:mas){
+            System.out.printf ( name + "[%-2d]=%-6.1f", col, mas[col] );
+            col++;
+            if (col%cols==0 || col == mas.length-1) {
+                System.out.println();
+            }
+        }
+    }
+}
+
---
+++ src/by/it/laurynovich/jd01_04/InOut.java
@@ -0,0 +1,25 @@
+package by.it.laurynovich.jd01_04;
+
+/**
+ * Created by vseotdala on 05.09.2016.
+ */
+public class InOut {
+    public static void printArr(double[][] m){
+        for (int i = 0 ; i < m.length; i++) {
+            for (int j = 0; j <m[0].length ; j++) {
+                System.out.printf("%-6.2f ",m[i][j]);
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+    }
+    public static void printArr(double[]x){
+        for (int i = 0; i < x.length ; i++) {
+            System.out.printf("%-10.2f", x[i]);
+            System.out.println();
+        }
+    }
+    //public static void printArray(double[][]x)
+
+}
