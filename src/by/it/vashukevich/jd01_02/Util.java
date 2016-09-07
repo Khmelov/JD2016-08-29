a78a8e9994c061910694b81f7fe64655eac67e66
Vashukevich <vladimir-vashukevich@tut.by>
2016-09-07 12:44:24 +0300

    jd01_02.....04

---
+++ src/by/it/vashukevich/jd01_02/Util.java
@@ -0,0 +1,29 @@
+package by.it.vashukevich.jd01_02;
+
+import java.util.Scanner;
+
+/**
+ * Created by Admin on 31.08.16.
+ */
+public class Util {
+    static public String readFromConsole(){
+        String result;
+       Scanner scanner = new Scanner(System.in);
+       result=scanner.nextLine();
+        return result;
+    }
+    static public int[] getArrayInt(String line){
+        String strArray[]=line.split(" ");
+        //split преобразовывает строку в массив целых данных
+        int mas[]=new int[strArray.length];
+        for (int i=0;i<strArray.length;i++){
+            mas[i]=Integer.parseInt(strArray[i]);
+        }
+        return mas;
+    }
+    static public void printArray(int[] mas){
+        for (int value:mas){
+            System.out.println(" "+value);
+        }
+    }
+}
---
+++ src/by/it/vashukevich/jd1_03/InOut.java
@@ -0,0 +1,78 @@
+package by.it.vashukevich.jd1_03;
+
+import java.util.Scanner;
+
+
+/**
+ * Created by Admin on 03.09.16.
+ */
+public class InOut {
+    public static String consoleReadLine() {
+        /*  InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+            BufferedReader line = new BufferedReader(inputStreamReader);
+            String out = line.readLine();
+            line.close();
+            return out;
+            }
+               */
+        String line;
+        Scanner scanner = new Scanner(System.in);
+        line = scanner.nextLine();
+        return line;
+    }
+
+    static public double[] stringLineToInArray(String line) {
+        line = line.trim();//удаляем пробелы в начале и в конце
+        String[] linString = line.split(" ");//преобразование строки в массив
+        int count = linString.length;//найдем длину массива
+        double[] mas = new double[count];//создали массив для чисел
+        for (int i = 0; i < count; i++) {
+            mas[i] = Double.parseDouble(linString[i]);
+        }
+        return mas;
+    }
+
+    public static void arrayPrint(double[] mas, String name, int cols) {
+        int col = 0;
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(name);//печатаем имя массива
+            System.out.printf("[% -3d]=%-4.1f", i, mas[i]);//печатаем знчения
+            col++;
+            if ((0 == col % cols)             //если конец колонки
+                    || (mas.length - 1 == i)) {  //или конец массива
+                System.out.println();//тогда напечатаем перевод строки
+            }
+
+        }
+    }
+
+    public static void arrayPrint2D(double[][] mas, String name) {
+        for (int i = 0; i < mas.length; i++) {      //перебираем строки массива
+            for (int j = 0; j < mas[0].length; j++) {  //перебираем столбцы мссива
+                System.out.printf(name + "[%1d,%1d]=%-6.1f", i, j, mas[i][j]);//печатаем имя массива
+                System.out.println();
+                }
+            System.out.println();
+            }
+        }
+
+    public static double[][] inArrayPrint2D() {
+        double [][]mass={
+                {1,2,3,4,},
+                {5,6,7,8,},
+                {9,10,11,12}
+        };
+        return mass;
+    }
+
+    public static void arrayPrint2D(double[][] mass) {
+        for (int i = 0; i < mass.length; i++) {      //перебираем строки массива
+            for (int j = 0; j < mass[0].length; j++) {  //перебираем столбцы мссива
+                System.out.printf("%6.1f",mass[i][j]);//печатаем имя массива
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
+
