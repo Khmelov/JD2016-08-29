63b5a35b6cba70cd07a15f82f95735aa4f4b9d64
wtsiamruk <wtsiamruk@gmail.com>
2016-09-09 00:12:51 +0300

    маленькие правки, task6 now done yet

---
+++ src/by/it/tsiamruk/jd01_05/math/Task6.java
@@ -0,0 +1,17 @@
+package by.it.tsiamruk.jd01_05.math;
+
+
+
+public class Task6 {
+    public static double[] newArray(){
+        //calculate quantity of c argument.
+        int count = 0;
+        for (double с = -1.5; с <= 6; с+=0.3){
+            count++;
+        }
+
+        //announce new array type double and assign aray lenght = count.
+        double[] array = new double[count];
+
+    }
+}
