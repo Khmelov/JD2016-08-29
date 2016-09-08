2f9c45a272c7c551990845d4c368b5be51f176ed
wtsiamruk <wtsiamruk@gmail.com>
2016-09-08 09:26:44 +0300

    Commit jd05

---
+++ src/by/it/tsiamruk/jd01_05/math/Task3.java
@@ -0,0 +1,20 @@
+package by.it.tsiamruk.jd01_05.math;
+
+import static java.lang.Math.*;
+
+public class Task3 {
+    public static void functionTabulation(){
+
+        final double rangeStart = 3.5;
+        final double rangeEnd = 25.5;
+
+        double x = 3.567;
+
+        System.out.println("I param I        value       I");
+
+        for (double a = rangeStart; a <= rangeEnd; a +=3.75){
+            double y = 1 / tan(pow(x, 3)) + 2.24 * a * x;
+            System.out.println(String.format("%-2s%-6s%-2s%-19s%-20s", "I", a, "I", y, "I"));
+        }
+    }
+}
