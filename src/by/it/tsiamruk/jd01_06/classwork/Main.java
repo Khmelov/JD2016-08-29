a5964eed27a98b096638ff89bdc9e8c9579d5b53
wtsiamruk <wtsiamruk@gmail.com>
2016-09-13 08:34:04 +0300

    Task 6 done

---
+++ src/by/it/tsiamruk/jd01_06/classwork/Main.java
@@ -0,0 +1,9 @@
+package by.it.tsiamruk.jd01_06.classwork;
+
+public class Main {
+    public static void main(String[] args){
+        TaskA.task1();
+        System.out.println();
+        TaskA.task2();
+    }
+}
