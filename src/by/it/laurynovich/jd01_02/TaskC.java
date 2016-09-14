fc98af88a92899809f6a1108c72db408f54c3bfd
kate.lavrinovich <kate.lavrinovich>
2016-09-14 12:42:43 +0300

    task jd01_02

--- src/by/it/laurynovich/jd01_02/TaskC.java
+++ src/by/it/laurynovich/jd01_02/TaskC.java
@@ -1,42 +1,51 @@
-//package by.it.laurynovich.jd01_02;
-//
-///**
-// * Created by vseotdala on 08.09.2016.
-// */
-//public class TaskC {
-//
-//    public static double[][] makeMatrix(int n) {
-//        double m[][] = new double[n][n];
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < n; j++) {
-//                m[i][j] = (int) (Math.random() * (2 * n + 1) - n);
-//            }
-//        }
-//        return m;
-//    }
-//
-//    //        1. Найти сумму элементов матрицы, расположенных между первым и
-////        вторым положительными элементами каждой строки.
-//    public static double[] getSum(double[][] mas) {
-//        double[] masSum = new double[mas.length];
-//        double sum = 0;
-//        int a = 0;
-//        int b = 0;
-//        for (int i = 0; i < mas.length; i++) {
-//            masSum[i] = 0;
-//            for (int j = 0; j < mas.length; j++) {
-//                if (mas[i][j] > 0) {
-//                    a = j;
-//                    if (j > a) {
-//                        b = j;
-//                    }
-//                }
-//            }
-//            for (int k = a + 1; k < b ; k++) {
-//                    masSum[i] = masSum[i] + mas[i][k];
-//            }
-//
-//        }
-//        return masSum;
-//    }
-//}
+package by.it.laurynovich.jd01_02;
+
+/**
+ * Created by vseotdala on 08.09.2016.
+ */
+public class TaskC {
+
+    public static double[][] makeMatrix(int n) {
+        double m[][] = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                m[i][j] = (int) (Math.random() * (2 * n + 1) - n);
+            }
+        }
+        return m;
+    }
+
+    //        1. Найти сумму элементов матрицы, расположенных между первым и
+//        вторым положительными элементами каждой строки.
+    public static double[] getSum(double[][] mas) {
+        double[] masSum = new double[mas.length];
+        //double sum = 0;
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < mas.length; i++) {
+            masSum[i] = 0;
+            for (int j = 0; j < mas.length; j++) {
+                if (mas[i][j] > 0) {
+                    a = j;
+                    System.out.println("masA="+mas[i][j]);
+                }
+                    if (a >= 0) {
+                        j++;
+                        if (mas[i][j] > 0)
+                        b = j;
+                        System.out.println("masb="+mas[i][j]);
+                }
+                if (a >= 0 && b >0){
+                    break;
+                }
+            }
+            System.out.println("a ="+a);
+            System.out.println("b = "+b);
+
+            for (int k = a + 1; k < b; k++) {
+                masSum[i] = masSum[i] + mas[i][k];
+            }
+        }
+        return masSum;
+    }
+}
---
+++ src/by/it/laurynovich/jd01_06/TaskB1.java
@@ -0,0 +1,32 @@
+package by.it.laurynovich.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+/**
+ * Created by vseotdala on 13.09.2016.
+ */
+
+//  1. Из текста удалить все слова длины 5, начинающиеся на согласную букву.
+public class TaskB1 {
+
+    public boolean Soglasnaja (String word){
+        String sdf = "йцкнгшщхъфвпрлджчсмтьб";
+        sdf = sdf + sdf.toUpperCase();
+        int len = word.length();
+        if (len == 5)
+        return (sdf.indexOf(word.charAt(0)) >= 0 );
+    }
+
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder(Data.POEM);
+        Pattern p = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher m = p.matcher(text);
+        while (m.find())
+            if ( Soglasnaja(m.group()))
+            {
+                System.out.println(m.group());
+            }
+
+    }
+
+}
