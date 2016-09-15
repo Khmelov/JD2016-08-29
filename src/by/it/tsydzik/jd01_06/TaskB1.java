b6f42ed7d05a1f0448f4bfd89f2f5d634e0029b8
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2016-09-15 12:53:43 +0300

    fixed Task B1

--- src/by/it/tsydzik/jd01_06/TaskB1.java
+++ src/by/it/tsydzik/jd01_06/TaskB1.java
@@ -1,30 +1,11 @@
 package by.it.tsydzik.jd01_06;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * @author Eugene Tsydzik
  * @since 9/14/16.
  */
 public class TaskB1 {
     public static void taskB1() {
-        StringBuilder sb = new StringBuilder(Data.POEM);
-        Pattern pattern = Pattern.compile("[а-яА-ЯЁё]{5,}");
-        Matcher matcher = pattern.matcher(sb);
-
-        try {
-            while (matcher.find()) {
-                int start = matcher.start();
-                if (matcher.group().length() == 5) {
-                    sb.delete(start, start + 5);
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("");
-        }
-
-
-        System.out.println(sb.toString());
+        System.out.println(Data.POEM.replaceAll("(?<![а-яА-ЯёЁ])[бБвВгГдДжЖзЗкКлЛмМнНпПрРсСтТфФхЧцЦчЧшШщЩ]{1}[а-яА-ЯЁё]{4}(?![а-яА-ЯёЁ])\\s", ""));
     }
 }
