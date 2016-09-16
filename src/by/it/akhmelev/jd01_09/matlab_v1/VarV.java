2a1c86d1b552e668498c8352ba2d0293d3c3f9db
akhmelev <akhmelev@gmail.com>
2016-09-16 12:27:10 +0300

    Add matlab v1

---
+++ src/by/it/akhmelev/jd01_09/matlab_v1/VarV.java
@@ -0,0 +1,36 @@
+package by.it.akhmelev.jd01_09.matlab_v1;
+
+import java.util.Arrays;
+
+public class VarV  extends Var{
+    double[] value;
+
+    public VarV(double[] value) {
+        this.value = value;
+    }
+
+    @Override
+    public Var add(Var var) {
+        if (var instanceof VarF) {
+            VarV res=new VarV(value);
+            for (int i = 0; i < value.length; i++) {
+               res.value[i]=res.value[i]+((VarF) var).value;
+            }
+            return res;
+        }
+
+        if (var instanceof VarV) {
+            VarV res=new VarV(value);
+            for (int i = 0; i < value.length; i++) {
+                res.value[i]=res.value[i]+((VarV) var).value[i];
+            }
+            return res;
+        }
+        return var.add(this);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(value);
+    }
+}
