f443792f3fa28aa48e146eaabaf3f389901dcfc8
Dmitry Emelyanov <mekswow@gmail.com>
2016-09-19 16:02:35 +0300

    jd01_08

---
+++ src/by/it/emelyanov/jd01_08/Mobile.java
@@ -0,0 +1,13 @@
+package by.it.emelyanov.jd01_08;
+
+public interface Mobile {
+    void switchOn();
+    void switchOff();
+    void checkBalance();
+    void setVolume();
+    void vibrateOn();
+    void vibrateOff();
+    void flightModeOn();
+    void flightModeOff();
+
+}
