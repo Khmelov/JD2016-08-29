bdc55b036cc04717075810bfae982133a6aceb28
senchenkoDV <senchenkodv@gmail.com>
2016-09-21 01:43:24 +0300

    jd01_02 red

--- src/by/it/senchenko/jd01_08/Player.java
+++ src/by/it/senchenko/jd01_08/Player.java
@@ -9,7 +9,7 @@ public abstract class Player implements IEquipment{
     private boolean pause = false; //плеер на паузе да/нет
 
     /**
-     *
+     *equipmentStatus()- возвращает текущее состояние плеера(исправен или нет)
      * @return
      */
     @Override
@@ -17,44 +17,79 @@ public abstract class Player implements IEquipment{
         return status;
     }
 
+    /**
+     * equipmentOn() - устанавливает флаги on и play на true
+     */
     @Override
     public void equipmentOn() {
         on = true;
         play = true;
     }
 
+    /**
+     * equipmentWorks() - возвращает текущее состояние плеера(работает или нет)
+     * @return
+     */
     @Override
     public boolean equipmentWorks() {
         return on;
     }
 
+    /**
+     * equipmentResult() - возвращает текущее состояние игры плеера(играет или нет)
+     * @return
+     */
     @Override
     public boolean equipmentResult() {
         return play;
     }
 
+    /**
+     * equipmentOff() - выключает плеер
+     */
     @Override
     public void equipmentOff() {
         on = false;
     }
 
+    /**
+     * equipmentBreak() - ломает плеер
+     */
     @Override
     public void equipmentBreak() {
         status = false;
     }
 
+    /**
+     * equipmentFix() - чинит плеер
+     */
     @Override
     public void equipmentFix() {
         status = true;
     }
 
+    /**
+     * pause() - меняет флаг, ставит плеер на паузу
+     */
     public void pause(){
-        play = !play;  //переключает состояние паузы
+        play = !play;
     }
 
-    public abstract void chooseFile(int file);    //реализовать метод выбрать файл
-    public abstract void volumeUp();    //реализовать метод увеличить громкость
-    public abstract void volumeDown();  //реализовать метод уменьшить громкость
+    /**
+     * chooseFile - принимает на вход целое значение, выбирает файл
+     * @param file
+     */
+    public abstract void chooseFile(int file);
+
+    /**
+     * volumeUp() - увеличивает громкость
+     */
+    public abstract void volumeUp();
+
+    /**
+     * volumeDown() - уменьшает громкость
+     */
+    public abstract void volumeDown();
     public boolean stateStatus(){return status;}
     public boolean stateOn(){return on;}
     public boolean statePlay(){return play;}
