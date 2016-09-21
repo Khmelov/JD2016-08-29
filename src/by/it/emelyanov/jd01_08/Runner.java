c1a296d4d771ee44c19700dc7c8d737d04208ca7
Dmitry Emelyanov <mekswow@gmail.com>
2016-09-21 14:12:06 +0300

    01_08 доработка

--- src/by/it/emelyanov/jd01_08/Runner.java
+++ src/by/it/emelyanov/jd01_08/Runner.java
@@ -2,14 +2,19 @@ package by.it.emelyanov.jd01_08;
 
 public class Runner {
     public static void main(String[] args) {
-        Model siemensC65 = new Model(4);
-        siemensC65.switchOn();
+        SiemensMobile siemensC65 = new Model(4);
         siemensC65.switchOff();
         siemensC65.checkBalance();
         siemensC65.flightModeOn();
-        siemensC65.flightModeOff();
         siemensC65.setVolume();
-        siemensC65.vibrateOn();
         siemensC65.vibrateOff();
+        System.out.println("///////////////////////////////////////////////////////////////////////////////////////");
+        SiemensMobile siemensC70 = new ModelS(10);
+        siemensC70.switchOn();
+        siemensC70.checkBalance();
+        siemensC70.flightModeOff();
+        siemensC70.setVolume();
+        siemensC70.vibrateOn();
+
     }
 }
