b2a924ee6213f57a5d70501bbd70bf450bfafd14
Vashukevich <vladimir-vashukevich@tut.by>
2016-09-21 11:47:37 +0300

    jd01_08 изменения

--- src/by/it/vashukevich/jd01_08/Runner.java
+++ src/by/it/vashukevich/jd01_08/Runner.java
@@ -5,12 +5,11 @@ package by.it.vashukevich.jd01_08;
  */
 public class Runner {
     public static void main(String[] args) {
-    ExtramuralStudent extramuralStudent=new ExtramuralStudent(7,8,9);
+    Student extramuralStudent=new ExtramuralStudent(7,8,9);
         extramuralStudent.extramural();
         extramuralStudent.work();
         extramuralStudent.learn();
         extramuralStudent.examination();
-        //extramuralStudent.toString();
         System.out.println(extramuralStudent.toString());
 
       /* System.out.println( extramuralStudent.showState());
