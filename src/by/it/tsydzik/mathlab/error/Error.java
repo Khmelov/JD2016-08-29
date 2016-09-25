ed70f2def88e1865b2fcf3f60c96c38ba1747eaf
etsydzik <tsydzik.eugene@gmail.com>
2016-09-25 21:44:57 +0300

    tsydzik.  mathlab. homework

---
+++ src/by/it/tsydzik/mathlab/error/Error.java
@@ -0,0 +1,11 @@
+package by.it.tsydzik.mathlab.error;
+
+/**
+ * @author Eugene Tsydzik
+ * @since 09/24/16.
+ */
+public class Error {
+	public Error(String str) {
+		System.out.println("ERROR: " + str);
+	}
+}
