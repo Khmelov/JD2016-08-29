0766678970397de560bd99cae9091d8b33e89a97
Vashukevich <vladimir-vashukevich@tut.by>
2016-09-25 20:43:57 +0300

    jd01_09 A и B( не полностью)

---
+++ src/by/it/vashukevich/jd01_09/mathlab/IOperation.java
@@ -0,0 +1,11 @@
+package by.it.vashukevich.jd01_09.mathlab;
+
+/**
+ * Created by Admin on 16.09.16.
+ */
+public interface IOperation {
+    Var add(Var var);
+    Var sub(Var var);
+    Var mul(Var var);
+    Var div(Var var);
+}
