c2cf584b74075732cc4c58083be9fefdcaaeba65
etsydzik <tsydzik.eugene@gmail.com>
2016-09-30 12:30:15 +0300

    jd01_15. taskB ->part

---
+++ src/by/it/tsydzik/jd01_15/Main.java
@@ -0,0 +1,11 @@
+package by.it.tsydzik.jd01_15;
+
+/**
+ * @author Eugene Tsydzik
+ * @since 9/30/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        TaskB.taskB();
+    }
+}
