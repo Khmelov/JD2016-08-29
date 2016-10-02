ea1cb6dd45c99231903993e4c55b5c9250740a83
Vashukevich <vladimir-vashukevich@tut.by>
2016-10-02 17:17:55 +0300

    jd01_13 A,B

---
+++ src/by/it/vashukevich/jd01_13/Util.java
@@ -0,0 +1,12 @@
+package by.it.vashukevich.jd01_13;
+
+import java.util.Scanner;
+
+/**
+ * Created by Admin on 26.09.16.
+ */
+public class Util {
+    static String oneLine() {
+return (new Scanner(System.in).nextLine());
+    }
+}
