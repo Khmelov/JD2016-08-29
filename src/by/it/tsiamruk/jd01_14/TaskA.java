0eb33f11fffbeb10e11c6e861f10cff3ec3870c5
wtsiamruk <wtsiamruk@gmail.com>
2016-10-03 02:00:31 +0300

    jd01_14 Task A

--- src/by/it/tsiamruk/jd01_14/TaskA.java
+++ src/by/it/tsiamruk/jd01_14/TaskA.java
@@ -10,7 +10,7 @@ import java.io.IOException;
  * Created by waldemar on 02/10/2016.
  */
 public class TaskA {
-    static void taskA() {
+    public static void main(String[] args) {
         //создание перменных для занесения в файл
         int[] variables = new int[20];
         for (int i = 0; i < variables.length; i++) {
@@ -18,31 +18,34 @@ public class TaskA {
         }
         //занесение в файл
         try (FileOutputStream fos = new FileOutputStream("/Users/waldemartsiamruk/test.txt");) {
-            for (int variable : variables) {
-                fos.write(variable);
+            for (int i = 0; i < variables.length; i++) {
+                fos.write(variables[i]);
             }
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден." + e.getMessage());
         } catch (IOException e) {
             System.out.println("Ошибка ввода/вывода" + e.getMessage());
         }
+        //вывод из файла
         try (FileInputStream fis = new FileInputStream("/Users/waldemartsiamruk/test.txt")) {
             System.out.println("Размер файла: " + fis.available() + " байт(а)");
             byte[] buffer = new byte[fis.available()];
             fis.read(buffer, 0, fis.available());
             System.out.print("Все Числа: ");
-            for (byte aBuffer1 : buffer) {
-                System.out.printf("%d ", (int) aBuffer1);
+            for (int i = 0; i < buffer.length; i++) {
+                System.out.printf("%d ", (int) buffer[i]);
             }
             double average = 0;
             if (buffer.length > 0) {
                 double sum = 0;
-                for (byte aBuffer : buffer) {
-                    sum += (int) aBuffer;
+                for (int i = 0; i < buffer.length; i++) {
+                    sum += (int) buffer[i];
                 }
                 average = sum / buffer.length;
             }
             System.out.println("\nСредне арифметическое: " + average);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
