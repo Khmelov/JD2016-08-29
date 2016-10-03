02c3443889ed97a69b98646b359dd129b8edf5e2
wtsiamruk <wtsiamruk@gmail.com>
2016-10-03 14:16:53 +0300

    jd01_14 Tasks A, B

---
+++ src/by/it/tsiamruk/jd01_14/TaskB.java
@@ -0,0 +1,41 @@
+package by.it.tsiamruk.jd01_14;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by waldemar on 03/10/2016.
+ */
+public class TaskB {
+    protected static void taskB() {
+        StringBuilder sb = new StringBuilder();
+        int countWords = 0;
+        int countPunct = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader("/Users/waldemartsiamruk/text.txt"))) {
+            String text;
+            while ((text = br.readLine()) != null) {
+                sb.append(text + "\n");
+            }
+            //System.out.println(sb.toString());
+            Pattern words = Pattern.compile("[aA-zZ]+");
+            Matcher wMatch = words.matcher(sb.toString());
+            Pattern punctiation = Pattern.compile("[\\.,?!;:]");
+            Matcher punctMatch = punctiation.matcher(sb.toString());
+            while (wMatch.find()) {
+                countWords++;
+            }
+            while (punctMatch.find()) {
+                countPunct++;
+            }
+            System.out.printf("Кол-во слов: %d \nКол-во знаков препинания: %d\n", countWords, countPunct);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
