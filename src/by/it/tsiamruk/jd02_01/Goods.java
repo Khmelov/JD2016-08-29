df0290b683d5e58f9b0e11d9346f08b22228cfdf
wtsiamruk <wtsiamruk@gmail.com>
2016-10-03 14:20:15 +0300

    jd01_14 Tasks A, B, little corrections

---
+++ src/by/it/tsiamruk/jd02_01/Goods.java
@@ -0,0 +1,28 @@
+package by.it.tsiamruk.jd02_01;
+
+import java.util.*;
+
+/**
+ * Created by waldemar on 03/10/2016.
+ */
+public class Goods {
+    private static Map<String, Double> table = new HashMap<String, Double>();
+    private static List<String> names;
+
+    static {
+        table.put("Bread", 1.0);
+        table.put("Milk", 1.0);
+        table.put("Yogurt", 1.0);
+        table.put("Sugar", 1.0);
+        table.put("Salt", 1.0);
+        table.put("Butter", 2.2);
+        table.put("Meat", 57.6);
+        table.put("Coffee", 12.0);
+        names = new ArrayList<>(table.keySet());
+
+    }
+
+    protected static String random() {
+        return names.get(Helper.rnd(names.size() - 1));
+    }
+}
