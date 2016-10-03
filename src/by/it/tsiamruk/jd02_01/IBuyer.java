9b6816698b789da133ed14c596ec4a8f2a9b9dc1
wtsiamruk <wtsiamruk@gmail.com>
2016-10-03 14:20:15 +0300

    jd01_14 Tasks A, B, little corrections

---
+++ src/by/it/tsiamruk/jd02_01/IBuyer.java
@@ -0,0 +1,12 @@
+package by.it.tsiamruk.jd02_01;
+
+/**
+ * Created by waldemar on 03/10/2016.
+ */
+public interface IBuyer {
+    void enterToMarket();
+
+    void chooseGoods();
+
+    void goToOut();
+}
