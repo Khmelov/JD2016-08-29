512ca40fd60450d4e97bef898fb865e6e2f713c9
SukoraStas <stasiksukora@mail.ru>
2016-10-04 23:53:35 +0300

    jd01_14

---
+++ src/by/it/sukora/jd01_14/HomeWork/TaskC.java
@@ -0,0 +1,32 @@
+package by.it.sukora.jd01_14.HomeWork;
+
+
+import java.io.File;
+import java.io.StringWriter;
+
+public class TaskC {
+    private String dir;
+
+    public TaskC() {
+        this.dir = System.getProperty("user.dir");
+    }
+
+    public TaskC(String subdir) {
+        this.dir = System.getProperty("user.dir") + subdir;
+    }
+
+    public void printFilesDirsSeparately() {
+        File f = new File(this.dir);
+        StringWriter dirs = new StringWriter();
+        StringWriter files = new StringWriter();
+        for (String name: f.list()) {
+            if ((new File(this.dir + "/" + name)).isDirectory()) dirs.write(name + "\n");
+            else if ((new File(this.dir + "/" + name)).isFile()) files.write(name + "\n");
+        }
+        System.out.printf("Directories of %s:\n", f.getAbsolutePath());
+        System.out.println(dirs.toString());
+
+        System.out.printf("Files of %s:\n", f.getAbsolutePath());
+        System.out.println(files.toString());
+    }
+}
