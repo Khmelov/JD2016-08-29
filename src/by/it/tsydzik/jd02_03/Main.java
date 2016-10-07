5ce81e7097293844134767b1a848af05aa01542f
etsydzik <tsydzik.eugene@gmail.com>
2016-10-07 13:15:18 +0300

    jd02_03 classwork

--- src/by/it/tsydzik/jd02_03/Main.java
+++ src/by/it/tsydzik/jd02_03/Main.java
@@ -6,18 +6,21 @@ package by.it.tsydzik.jd02_03;
  */
 public class Main {
     public static void main(String[] args) {
-        int countBuyer = 0;
-        while (countBuyer < 20) {
-            int n = Helper.rnd(10);
+
+        Dispatcher.poolCashier.execute(new Cashier());
+
+        while (Dispatcher.countBuyers.get() < Dispatcher.PLAN_COUNT_BUYERS) {
+            int n = Helper.rnd(2);
             for (int i = 0; i < n; i++) {
-                Buyer buyer = new Buyer(++countBuyer);
+                Buyer buyer = new Buyer(Dispatcher.countBuyers.incrementAndGet());
                 Thread th = new Thread(buyer);
                 th.start();
-                if (countBuyer == 20) {
+                if (Dispatcher.countBuyers.get() == Dispatcher.PLAN_COUNT_BUYERS) {
                     break;
                 }
             }
             Helper.sleep(1000);
         }
+        Dispatcher.poolCashier.shutdown();
     }
 }
