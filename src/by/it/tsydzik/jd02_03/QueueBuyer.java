97fdb119fb9e0386d79f708a22437238a7549a0a
etsydzik <tsydzik.eugene@gmail.com>
2016-10-07 13:15:18 +0300

    jd02_03 classwork

--- src/by/it/tsydzik/jd02_03/QueueBuyer.java
+++ src/by/it/tsydzik/jd02_03/QueueBuyer.java
@@ -11,13 +11,18 @@ public class QueueBuyer {
 
     private static ArrayDeque<Buyer> queue = new ArrayDeque<>();
 
+    public static int getSize() {
+        int res = 0;
+        synchronized (monitorQueueBuyer) {
+            res = queue.size();
+        }
+        return res;
+    }
+
     public static void add(Buyer buyer) {
         queue.addLast(buyer);
-        if (Dispatcher.countCashier < 5) {
-            synchronized (Dispatcher.monitorCountCashier) {
-                new Thread(new Cashier()).start();
-                Dispatcher.countCashier++;
-            }
+        if (Dispatcher.needCashier()) {
+            new Thread(new Cashier()).start();
         }
     }
 
@@ -32,7 +37,7 @@ public class QueueBuyer {
     static boolean needService() {
         boolean needService;
         synchronized (monitorQueueBuyer) {
-            needService =! queue.isEmpty();
+            needService = !queue.isEmpty();
         }
         return needService;
     }
