f32fc96136d891402bc630d3b893752f3d2c7c0d
etsydzik <tsydzik.eugene@gmail.com>
2016-10-07 13:15:18 +0300

    jd02_03 classwork

--- src/by/it/tsydzik/jd02_03/Cashier.java
+++ src/by/it/tsydzik/jd02_03/Cashier.java
@@ -4,25 +4,35 @@ package by.it.tsydzik.jd02_03;
  * @author Eugene Tsydzik
  * @since 10/05/16.
  */
-public class Cashier implements Runnable{
+public class Cashier implements Runnable {
+    int number;
+
+    public Cashier() {
+        number = Dispatcher.countCashier.incrementAndGet();
+    }
 
     @Override
     public void run() {
-        while (QueueBuyer.needService()){
-            Buyer buyer = QueueBuyer.extract();
-            System.out.println("Start service " + buyer);
-            Helper.sleep(Helper.rnd(1000));
-            System.out.println("Billing " + buyer);
-            System.out.println("Stop service " + buyer);
-            synchronized (buyer){
-                buyer.notify();
+        do {
+            if (QueueBuyer.needService()) {
+                Buyer buyer = QueueBuyer.extract();
+                System.out.println(this + " ... Start " + buyer);
+                Helper.sleep(Helper.rnd(1000));
+                System.out.println(this + " ... Billing " + buyer);
+                System.out.println(this + " ... Stop " + buyer);
+                synchronized (buyer) {
+                    buyer.notify();
+                }
             }
         }
-
-        synchronized (Dispatcher.monitorCountCashier){
-            Dispatcher.countCashier--;
-        }
+        while (Dispatcher.needCashier());
+        Dispatcher.countCashier.decrementAndGet();
 
         System.out.println("-------->>>>>>>>>>>>>> STOP");
+    }
+
+    @Override
+    public String toString() {
+        return "Cashier № " + number;
     }
 }
