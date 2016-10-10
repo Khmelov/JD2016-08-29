a579a96a3a9a1222158d3d54bb73ebcc71acd84c
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2016-10-11 00:44:34 +0300

    jd02_03Training

---
+++ src/by/it/tsydzik/jd02_03Training/Helper.java
@@ -0,0 +1,23 @@
+package by.it.tsydzik.jd02_03Training;
+
+/**
+ * @author Eugene Tsydzik
+ * @since 10/11/16.
+ */
+public class Helper {
+    public static int rnd(int from, int to) {
+        return from + (int) (Math.round(Math.random() * (to - from)));
+    }
+
+    public static int rnd(int max) {
+        return rnd(0, max);
+    }
+
+    public static void sleep(int timeout) {
+        try {
+            Thread.sleep(timeout);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
