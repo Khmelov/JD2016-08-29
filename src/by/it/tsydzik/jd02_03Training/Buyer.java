a3f2a1a2707bb0bac0967317ca77ef83ca9fca81
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2016-10-11 00:44:34 +0300

    jd02_03Training

---
+++ src/by/it/tsydzik/jd02_03Training/Buyer.java
@@ -0,0 +1,36 @@
+package by.it.tsydzik.jd02_03Training;
+
+/**
+ * @author Eugene Tsydzik
+ * @since 11.10.16.
+ */
+public class Buyer implements Runnable, IBuyer {
+
+    int num;                    //номер покупателя
+    public boolean iWait=false; //флаг того, что покупатель в ожидании
+
+    @Override
+    public void enterMarket() {
+
+    }
+
+    @Override
+    public void chooseGoods() {
+
+    }
+
+    @Override
+    public void goToQueue() {
+
+    }
+
+    @Override
+    public void goToOut() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
