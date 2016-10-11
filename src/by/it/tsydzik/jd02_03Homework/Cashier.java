efeee07ff31cd51f183ed112792bc61da110a314
etsydzik <tsydzik.eugene@gmail.com>
2016-10-12 01:02:35 +0300

    jd02_03. homework

--- src/by/it/tsydzik/jd02_03Homework/Cashier.java
+++ src/by/it/tsydzik/jd02_03Homework/Cashier.java
@@ -13,21 +13,26 @@ public class Cashier implements Runnable {
 
     @Override
     public void run() {
-        do {
-            if (QueueBuyers.needService()) {
-                Buyer buyer = QueueBuyers.poll();
-                System.out.println(this + " начал обслуживание " + buyer);
-                Helper.sleep(Helper.rnd(1000));
-                if (buyer != null) {
-                    System.out.println(this + " закончил обслуживание " + buyer);
-                    synchronized (buyer) {
-                        buyer.notify();
-                    }
+        System.out.println(this + " открыл кассу");
+        while (!Dispatcher.finish()) {
+
+            Buyer buyer = QueueBuyers.poll();
+            if (buyer != null) {
+                synchronized (buyer) {
+                    System.out.println(this + " обслуживает клиента: " + buyer);
+                    Helper.sleep(Helper.rnd(2000, 5000));
+                    System.out.println(this + " обслужил клиента: " + buyer);
+
+                    //увеличим счетчик обслуженных клиентов
+                    Dispatcher.countCompleteBuyers.incrementAndGet();
+                    buyer.iWait = false;
+                    buyer.notify();     //отдаем блокировку обратно покупателю
                 }
+            } else {
+                Helper.sleep(1000);
             }
-        } while (Dispatcher.needCashiers());
-        System.out.println(this + " закрылся");
-        Dispatcher.countCashiers.decrementAndGet();
+        }
+        System.out.println(this + " закрыл кассу");
     }
 
     @Override
