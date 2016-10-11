3373a54a4979216533e5c0d8569c05eefca2dcb8
etsydzik <tsydzik.eugene@gmail.com>
2016-10-11 23:11:43 +0300

    jd02_03. Task A-B(partially)

--- src/by/it/tsydzik/jd02_03Training/Buyer.java
+++ src/by/it/tsydzik/jd02_03Training/Buyer.java
@@ -6,31 +6,64 @@ package by.it.tsydzik.jd02_03Training;
  */
 public class Buyer implements Runnable, IBuyer {
 
-    int num;                    //номер покупателя
-    public boolean iWait=false; //флаг того, что покупатель в ожидании
+    int number;                    //номер покупателя
+    public boolean iWait = false; //флаг того, что покупатель в ожидании
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Buyer(int number) {
+        this.number = number;
+        this.setName("Покупатель №" + number);
+    }
 
     @Override
-    public void enterMarket() {
+    public String toString() {
+        return "Покупатель №" + number;
+    }
 
+    @Override
+    public void enterToMarket() {
+        System.out.println(this + " вошел в магазин");
     }
 
     @Override
     public void chooseGoods() {
-
+        Helper.sleep(Helper.rnd(500, 1500));
+        System.out.println(this + " выбрал товар");
     }
 
     @Override
     public void goToQueue() {
-
+        QueueBuyers.add(this);
+        System.out.println(this + " добавлен в очередь");
+        synchronized (this) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void goToOut() {
-
+        System.out.println(this + " вышел из магазина");
     }
 
     @Override
     public void run() {
-
+        enterToMarket();
+//        takeBacket();
+        chooseGoods();
+        goToQueue();
+        goToOut();
     }
 }
