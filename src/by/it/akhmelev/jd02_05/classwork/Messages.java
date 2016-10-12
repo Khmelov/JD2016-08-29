2bca3e131a239813a415b164e242501949ce1afe
akhmelev <akhmelev@gmail.com>
2016-10-12 12:13:15 +0300

    jd02-05 classroom i18n l10n g11n

---
+++ src/by/it/akhmelev/jd02_05/classwork/Messages.java
@@ -0,0 +1,7 @@
+package by.it.akhmelev.jd02_05.classwork;
+
+public interface Messages {
+   String MESSAGE_WELCOME = "message.welcome";
+   String MESSAGE_USERNAME = "message.username";
+   String MESSAGE_QUESTION = "message.question";
+}
