5ffaf68e87a33df33ae7c3234796ea9e098e1bf1
nsavelyeva <natallia.savelyeva@gmail.com>
2016-10-13 19:47:19 +0300

    txt2properties.sh updated in jd_02_05 (i18n)

--- src/by/it/savelyeva/jd_02_05/txt2properties.sh
+++ src/by/it/savelyeva/jd_02_05/txt2properties.sh
@@ -6,6 +6,3 @@ for file in $LANGPATH/language*.txt;
     do echo "Processing $file ..";
     native2ascii -encoding utf-8 $file ${file%.txt}.properties;
 done;
-#native2ascii -encoding utf-8 $LANGPATH/language.txt $LANGPATH/language.properties
-#native2ascii -encoding utf-8 $LANGPATH/language_be_BY.txt $LANGPATH/language_be_BY.properties
-#native2ascii -encoding utf-8 $LANGPATH/language_ru_RU.txt $LANGPATH/language_ru_RU.properties
