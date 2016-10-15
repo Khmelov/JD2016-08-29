0a9262625c1a9b005639189aa8959a840769b3d4
etsydzik <tsydzik.eugene@gmail.com>
2016-10-15 22:03:30 +0300

    jd01_12. refactor TaskC

--- src/by/it/tsydzik/jd01_12/TaskA3.java
+++ src/by/it/tsydzik/jd01_12/TaskA3.java
@@ -3,6 +3,7 @@ package by.it.tsydzik.jd01_12;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * @author Eugene Tsydzik
@@ -11,8 +12,16 @@ import java.util.Collections;
 public class TaskA3 {
     public static void task() {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, -1, 0, -2, -3, -4, -5));
-        Collections.sort(list);
-        Collections.reverse(list);
+
+        long start = System.nanoTime();
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        long end = System.nanoTime();
+        System.out.printf("Computation time: %d ns\n", (end -start));
         System.out.println(list);
     }
 }
