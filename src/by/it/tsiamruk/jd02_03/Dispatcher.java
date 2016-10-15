ab9536728b15b23295da51e3ee6871aca27e37f8
wtsiamruk <wtsiamruk@gmail.com>
2016-10-16 00:29:45 +0300

    jd02_03 C in progress.

--- src/by/it/tsiamruk/jd02_03/Dispatcher.java
+++ src/by/it/tsiamruk/jd02_03/Dispatcher.java
@@ -2,9 +2,7 @@ package by.it.tsiamruk.jd02_03;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- * Created by user on 05.10.2016.
- */
+
 public class Dispatcher {
     static int countCashiers = 0;
     private static final int planCount = 40;
