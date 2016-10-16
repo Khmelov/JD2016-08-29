ca50eb2bc9a564c9ab9b53432579709a430fd943
wtsiamruk <wtsiamruk@gmail.com>
2016-10-16 13:09:07 +0300

    jd02_03 C in progress.

--- src/by/it/tsiamruk/jd02_03/QueueBuyers.java
+++ src/by/it/tsiamruk/jd02_03/QueueBuyers.java
@@ -1,21 +1,24 @@
 package by.it.tsiamruk.jd02_03;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 class QueueBuyers {
 
-    protected static ConcurrentLinkedQueue<Buyer> queue = new ConcurrentLinkedQueue<>();
+    protected static ConcurrentLinkedDeque<Buyer> queue = new ConcurrentLinkedDeque<>();
 
     public static void add(Buyer b) {
         if (queue.size() < 40)
-            queue.add(b);
+            if (b.isPensioner())
+                queue.addFirst(b);
+            else
+                queue.add(b);
     }
 
     static Buyer pool() {
         if (queue.isEmpty())
             return null;
         else
-            return queue.poll();
+            return queue.pollFirst();
     }
 
     protected static boolean needService() {
