2a650ed80128faf1a9d7173438adac7dcdd11fce
wtsiamruk <wtsiamruk@gmail.com>
2016-10-16 13:57:59 +0300

    jd02_03 C.

--- src/by/it/tsiamruk/jd02_03/Runner.java
+++ src/by/it/tsiamruk/jd02_03/Runner.java
@@ -2,18 +2,24 @@ package by.it.tsiamruk.jd02_03;
 
 public class Runner {
     public static void main(String[] args) {
-        //TODO: сделать так чтобы число покупателей изменялось - менее 10 в начале каждой минуты, и до 40 на 30 секунде каждой минуты
+        long t1 = System.currentTimeMillis();
+        double t = 0;
         while (!Dispatcher.planComplete()) {
             Dispatcher.needCashiers();
-            Helper.sleep(1000);
-            int n = Helper.rnd(2);
-            for (int i = 0; i < n; i++) {
+            int max = 0;
+            int buyersInside = Dispatcher.countBuyers.get() - Dispatcher.completeBuyersCount.get();
+            if (Math.round(t) % 60 < 30) max = 2;
+            else if (buyersInside <= 40 + (30 - t)) max = 2;
+            for (int i = 0; i < Helper.rnd(max); i++) {
                 Buyer buyer = new Buyer(Dispatcher.countBuyers.incrementAndGet());
                 Thread th = new Thread(buyer);
                 th.start();
                 if (Dispatcher.planComplete())
                     break;
             }
+            Helper.sleep(1000);
+            if (t > 60) t1 = System.currentTimeMillis();
+            t = (System.currentTimeMillis() - t1) / 1000;
         }
         Cashier.executor.shutdown();
     }
