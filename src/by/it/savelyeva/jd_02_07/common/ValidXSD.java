0447b8c824ca36fbee8b175dcd733d882c71e907
nsavelyeva <natallia.savelyeva@gmail.com>
2016-10-17 20:36:59 +0300

    jd_02_07 (Tasks A, B, C for Variant "Rent A Car" + classroom work in "common")

---
+++ src/by/it/savelyeva/jd_02_07/common/ValidXSD.java
@@ -0,0 +1,35 @@
+package by.it.savelyeva.jd_02_07.common;
+
+import org.xml.sax.SAXException;
+import javax.xml.*;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class ValidXSD {
+
+	public static void main(String[ ] args) {
+        String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+        String fileName =   "src/by/it/savelyeva/jd_02_07/common/04+XSD.xml";
+        String schemaName = "src/by/it/savelyeva/jd_02_07/common/04.xsd";
+        SchemaFactory factory = SchemaFactory.newInstance(language);
+        File schemaLocation = new File(schemaName);
+        try {
+            Schema schema = factory.newSchema(schemaLocation);
+            Validator validator = schema.newValidator();
+            Source source = new StreamSource(fileName);
+            validator.validate(source);
+            System.out.println(fileName + " is valid.");
+        } catch (SAXException e) {
+            System.err.print("Validation for "+ fileName + " is failed: "
+                    + e.getMessage());
+        } catch (IOException e) {
+            System.err.print(fileName + " is not valid:"
+                    + e.getMessage());
+        }
+    }
+}
