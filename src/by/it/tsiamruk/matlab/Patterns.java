56c3e9c82bfabf74131fc2f15e8466b233317820
wtsiamruk <wtsiamruk@gmail.com>
2016-10-21 19:26:30 +0300

    jd02_04 работают вектора матрицы и числа

--- src/by/it/tsiamruk/matlab/Patterns.java
+++ src/by/it/tsiamruk/matlab/Patterns.java
@@ -3,7 +3,7 @@ package by.it.tsiamruk.matlab;
 public class Patterns {
     //регулярки
     static public String exVal = "-?[0-9.]+";                                //числа
-    static public String exVec = "\\{(\\d*\\.?\\d+\\,?\\s?)+\\}";                       //вектора
+    static public String exVec = "\\{(-?\\d*\\.?\\d+\\,?\\s?)+\\}";                       //вектора
     static public String exMat = "\\{\\(.+\\),?}";                           //матрицы
     static public String exAny = "(" + exMat + ")|(" + exVec + ")|(" + exVal + ")";       //одно из...
     static public String exOper = "[-+*/]";                                   //операция
