7f9abdda7f784c249423fa4e7f5f845a22c6c0e6
wtsiamruk <wtsiamruk@gmail.com>
2016-10-25 16:20:54 +0300

    jd02_08 A

---
+++ src/by/it/tsiamruk/jd02_08/Run.java
@@ -0,0 +1,12 @@
+package by.it.tsiamruk.jd02_08;
+
+/**
+ * Created by waldemar on 25/10/2016.
+ */
+public class Run {
+    public static void main(String[] args) {
+        String path = System.getProperty("user.dir").concat("/src/by/it/tsiamruk/");
+        String filePath = path + "jd02_07/paymentSystem.xml";
+        SAXPars.saxParse(filePath);
+    }
+}
