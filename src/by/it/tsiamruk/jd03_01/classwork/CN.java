354678bd11d9be44000bc79b0587ecc0baa82a76
wtsiamruk <wtsiamruk@gmail.com>
2016-10-25 23:41:13 +0300

    rebuild project

--- src/by/it/tsiamruk/jd03_01/classwork/CN.java
+++ src/by/it/tsiamruk/jd03_01/classwork/CN.java
@@ -5,8 +5,8 @@ public class CN {
     //Обычно это файлы конфигурации сервера или фреймворка
     //а сам конфиг в этом случае - bean с инициализацией из XML
     //ТАК ЧТО ЭТО ЛИШЬ ПРИМЕР, ТАК НИКОГДА (!) НЕ ДЕЛАЮТ !
-    public static final String URL_DB = "jdbc:mysql://127.0.0.1:3306/wtsiamruk"+
+    public static final String URL_DB = "jdbc:mysql://127.0.0.1:2016/wtsiamruk"+
                                         "?useUnicode=true&amp;characterEncoding=UTF-8";
     public static final String USER_DB = "root";
-    public static final String PASSWORD_DB = "root";
+    public static final String PASSWORD_DB = "";
 }
