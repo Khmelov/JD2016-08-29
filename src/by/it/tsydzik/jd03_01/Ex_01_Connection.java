20113a0a0978000ce26be7907052c031276fda18
etsydzik <tsydzik.eugene@gmail.com>
2016-10-26 11:12:28 +0300

    jd03_01. fixed import

--- src/by/it/tsydzik/jd03_01/Ex_01_Connection.java
+++ src/by/it/tsydzik/jd03_01/Ex_01_Connection.java
@@ -1,6 +1,5 @@
 package by.it.tsydzik.jd03_01;
 
-import by.it.akhmelev.jd03_01.CN;
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
 
 import java.sql.Connection;
@@ -11,23 +10,24 @@ import java.sql.SQLException;
 public class Ex_01_Connection {
 
 
-    public static void main(String[ ] args) {
+    public static void main(String[] args) {
         try {
             Driver driver = new FabricMySQLDriver();
             DriverManager.registerDriver(driver);
-        } catch (SQLException e) {e.printStackTrace();}
-        try (Connection connection=
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try (Connection connection =
                      DriverManager.getConnection
-                             (by.it.tsydzik.jd03_01.CN.URL_DB, by.it.tsydzik.jd03_01.CN.USER_DB, by.it.tsydzik.jd03_01.CN.PASSWORD_DB);){
-                if (!connection.isClosed())
-                    System.out.println("Соединение установлено");
-                connection.close();
-                if (connection.isClosed())
-                    System.out.println("Соединение прервано");
-            }
-            catch (Exception e){
-                e.printStackTrace();
-            }
+                             (CN.URL_DB, CN.USER_DB, CN.PASSWORD_DB);) {
+            if (!connection.isClosed())
+                System.out.println("Соединение установлено");
+            connection.close();
+            if (connection.isClosed())
+                System.out.println("Соединение прервано");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
