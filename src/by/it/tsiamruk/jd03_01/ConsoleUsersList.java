57f682843e31942d3b139663d6e6159bdd9cbbb7
wtsiamruk <wtsiamruk@gmail.com>
2016-10-27 18:29:28 +0300

    03_01 03_02

---
+++ src/by/it/tsiamruk/jd03_01/ConsoleUsersList.java
@@ -0,0 +1,28 @@
+package by.it.tsiamruk.jd03_01;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Locale;
+
+/**
+ * Created by waldemar on 27/10/2016.
+ */
+public class ConsoleUsersList {
+    static void readUsersList() throws SQLException{
+        String sqlRequest = "SELECT * FROM users, role WHERE users.FK_role = role.ID;";
+
+        try(Connection connection = ConnectionCreator.getConnection();
+            Statement statement = connection.createStatement();) {
+            ResultSet resultSet = statement.executeQuery(sqlRequest);
+            while (resultSet.next()){
+                System.out.printf(Locale.ENGLISH, "ID = %d | Login: %s | Password: %s | Email: %s | RoleID: %d| RoleName: %s %n",
+                        resultSet.getInt("ID"),resultSet.getString("login"),resultSet.getString("password"),resultSet.getString("email"),resultSet.getInt("FK_role"),resultSet.getString("role"));
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
