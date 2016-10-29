25535fc69563a8df84433c8d784dcb58cf719d1c
Sukora-Stas <stasiksukora@mail.ru>
2016-10-29 22:41:08 +0300

    jd02_06

---
+++ src/by/it/sukora/jd02_06/VarFactoryIF.java
@@ -0,0 +1,11 @@
+package by.it.sukora.jd02_06;
+
+import by.it.sukora.jd02_04.matlab_with_parser.vars.Var;
+
+
+/**
+ * Created by Sukora Stas.
+ */
+public interface VarFactoryIF {
+    Var createVar(String str);//создание операнда
+}
