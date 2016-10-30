f4041c9151b40be471bb110ebef522b55a0b6f08
etsydzik <tsydzik.eugene@gmail.com>
2016-10-31 01:29:08 +0300

    jd03_03. Fixed ClientDAO, DAO, CustomRunner

--- src/by/it/tsydzik/jd03_03/CustomRunner.java
+++ src/by/it/tsydzik/jd03_03/CustomRunner.java
@@ -1,43 +1,40 @@
 package by.it.tsydzik.jd03_03;
 
 
+import by.it.tsydzik.jd03_03.beans.Client;
+import by.it.tsydzik.jd03_03.custom_dao.ClientDAO;
 import by.it.tsydzik.jd03_03.custom_dao.DAO;
-import by.it.tsydzik.jd03_03.custom_dao.UserDAO;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class CustomRunner {
 
-    static void showUsers(){
+    static void showUsers() {
         //покажем все, что есть в таблице пользователей
         System.out.println("\nТаблица пользователей:");
-        List<User> users=new UserDAO().getAll("");
-        for (User each:users) System.out.println(each);
+        List<Client> clients = new ClientDAO().getAll("");
+        for (Client each : clients) System.out.println(each);
     }
 
     public static void main(String[] args) throws SQLException {
         //инициализация работы с базой
         DAO dao = DAO.getDAO();
-        System.out.println("Роли пользователей");
-        for (Role role: dao.role.getAll("")) {
-            System.out.println(role);
-        }
+
+        //TODO нельзя вводить русские буквы. КАК ИСПРАВИТЬ??????????
+        Client client = new Client(0, "eugene");
+        //добавим клиента
+        if (dao.client.create(client)) System.out.println("\nДобавлен:" + client);
         showUsers();
 
-        User user = new User(0,"Сергей Иванович", "siivanov", "siivanov@mail.ru",2);
-        //добавим пользователя
-        if (dao.user.create(user)) System.out.println("\nДобавлен:" + user);
-        showUsers();
-
-        //извлечем и обновим пользователя с email siivanov@mail.ru
-        user = dao.user.getAll("WHERE ID>2").get(0);
-        user.setLogin("up_siivanov");
-        if (dao.user.update(user)) System.out.println("\nИзменен:" + user);
+        //извлечем и обновим клиента с email siivanov@mail.ru
+        client = dao.client.getAll("WHERE ID<2").get(0);
+        client.setName("dmitry");
+        if (dao.client.update(client)) System.out.println("\nИзменен:" + client);
         showUsers();
 
         //удалим пользователя
-        if (dao.user.delete(user)) System.out.println("\nУдален:" + user);
+        if (dao.client.delete(client)) System.out.println("\nУдален:" + client);
         showUsers();
     }
 }
