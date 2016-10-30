5ee46c27744770a6fce6ab3bb3824b36dd39eda4
wtsiamruk <wtsiamruk@gmail.com>
2016-10-30 19:41:20 +0300

    processed

--- src/by/it/tsiamruk/jd03_03/AbstractDAO.java
+++ src/by/it/tsiamruk/jd03_03/AbstractDAO.java
@@ -1,18 +1,19 @@
 package by.it.tsiamruk.jd03_03;
 
 
+import by.it.tsiamruk.jd03_03.Logger;
 import by.it.tsiamruk.jd03_01.connection.ConnectionCreator;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.logging.Logger;
+
 
 /**
  * Created by waldemar on 30/10/2016.
  */
 public class AbstractDAO {
-    private static Logger log = Logger.getLogger(AbstractDAO.class.getName());
+    private static Logger logger;
     protected int executeUpdate(String sql){
         int result = -1;
         try (Connection connection = ConnectionCreator.getConnection();
@@ -23,7 +24,7 @@ public class AbstractDAO {
                 if (resultSet.next()) result = resultSet.getInt(1);
             }
         } catch (Exception e) {
-           log.info(e.getMessage());
+            logger.writeInLog(e.getMessage(),e);
         }
         //System.out.println(result+":"+sql); //проверить SQL можно снимая комментарий с этой строки
         return result;
