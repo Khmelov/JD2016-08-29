558057c9fb428577f5abec535a94468bc62deadd
etsydzik <tsydzik.eugene@gmail.com>
2016-10-30 23:37:50 +0300

    jd03_03. Added beans, connectionCreator, AbstractDAO, InterfaceDAO

---
+++ src/by/it/tsydzik/jd03_03/beans/Rent.java
@@ -0,0 +1,17 @@
+package by.it.tsydzik.jd03_03.beans;
+
+import java.util.Date;
+
+/**
+ * @author Eugene Tsydzik
+ * @since 30.10.16.
+ */
+public class Rent {
+    private int id;
+    private Date startTime;
+    private Date endTime;
+    private int clientId;
+    private int autoId;
+
+
+}
