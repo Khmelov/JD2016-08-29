d90bbe8ff8a42ffde036ea2d741847c35546a02a
Dmitry Emelyanov <mekswow@gmail.com>
2016-10-30 16:39:07 +0300

    ref

--- src/by/it/emelyanov/jd03_02/SearchByRole.java
+++ src/by/it/emelyanov/jd03_02/SearchByRole.java
@@ -8,9 +8,9 @@ public class SearchByRole {
     public static void searchId(String role) throws SQLException {
         try (Connection connection = ConnectionCreator.getConnection();
              Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery("SELECT roles.ID FROM emelyanov.roles WHERE roles.Name ='" + role + "'");
+            ResultSet resultSet = statement.executeQuery("SELECT roles.ID FROM roles WHERE roles.Name ='" + role + "'");
             if (resultSet.next()) {
-                System.out.println(resultSet.getInt("ID"));
+                System.out.printf("id роли: " +role+ " = %s",resultSet.getInt("ID"));
             }
         }
     }
