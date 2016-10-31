3c635a436194f568635a56279bd2a242b0c814b1
etsydzik <tsydzik.eugene@gmail.com>
2016-10-31 12:20:08 +0300

    jd03_03. Added project build

---
+++ src/by/it/tsydzik/project/java/sample_controller/CommandError.java
@@ -0,0 +1,10 @@
+package by.it.tsydzik.project.java.sample_controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+class CommandError implements ActionCommand {
+    @Override
+    public String execute(HttpServletRequest request) {
+        return Actions.ERROR.jsp;
+    }
+}
