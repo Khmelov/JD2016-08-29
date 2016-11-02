46e5bacbbf71b1f6693d36285c0a769130fbf265
akhmelev <akhmelev@gmail.com>
2016-11-02 11:14:34 +0300

    ClassWork JD03_05_v1

---
+++ src/by/it/akhmelev/project/java/controller/CmdLogin.java
@@ -0,0 +1,10 @@
+package by.it.akhmelev.project.java.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class CmdLogin extends Action {
+    @Override
+    Action execute(HttpServletRequest req) {
+        return null;
+    }
+}
