4c61de42dfa65d7bb1402b2fd627e2e1702b29e9
etsydzik <tsydzik.eugene@gmail.com>
2016-11-02 11:33:32 +0300

    jd03_05. Controller

--- src/by/it/tsydzik/project/java/controller/FrontController.java
+++ src/by/it/tsydzik/project/java/controller/FrontController.java
@@ -8,17 +8,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class FrontController extends HttpServlet {
+
+    public RequestDispatcher disp(Action action) {
+        return getServletContext().getRequestDispatcher(action.getJsp());
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Action action = Actions.defineFrom(req);
         action.execute(req);
-        RequestDispatcher disp = getServletContext().getRequestDispatcher(action.getJsp());
-        disp.forward(req, resp);
-
+        disp(action).forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        Action action = Actions.defineFrom(req);
+        Action redirectAction = action.execute(req);
+        if (redirectAction != null) {
+            resp.sendRedirect("do?command=" + redirectAction);
+        } else {
+            disp(action).forward(req, resp);
+        }
     }
 }
