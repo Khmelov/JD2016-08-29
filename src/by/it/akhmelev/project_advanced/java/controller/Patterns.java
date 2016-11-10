5981899efa905364a6c0234be8f324169e20e818
Alex <akhmelev@gmail.com>
2016-11-11 00:01:26 +0300

    jd03_08 Summary && Advanced information about Ant Tomcat JSTL EL Listeners PoolConn etc...

---
+++ src/by/it/akhmelev/project_advanced/java/controller/Patterns.java
@@ -0,0 +1,8 @@
+package by.it.akhmelev.project_advanced.java.controller;
+
+public interface Patterns {
+    String LOGIN="[а-яА-ЯёЁa-zA-Z0-9-_.]+";
+    String PASSWORD="[а-яА-ЯёЁa-zA-Z0-9-_.]+";
+    String EMAIL="[a-zA-Z0-9-_.@]+";
+
+}
