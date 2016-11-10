94d6b612cf28c59e16b14899386f9adf2a3139f0
senchenkoDV <senchenkodv@gmail.com>
2016-11-10 19:33:32 +0300

    project v3

--- src/by/it/senchenko/project/java/controller/CmdLogin.java
+++ src/by/it/senchenko/project/java/controller/CmdLogin.java
@@ -15,8 +15,8 @@ public class CmdLogin extends Action {
         if (Form.isPost(req)) {
             User user = new User();
             try {
-                user.setLogin(Form.getParameter(req, "Login", Patterns.LOGIN));
-                user.setPassword(Form.getParameter(req, "Password", Patterns.PASSWORD));
+                user.setLogin(Form.getString(req, "Login", Patterns.LOGIN));
+                user.setPassword(Form.getString(req, "Password", Patterns.PASSWORD));
                 SingletonDAO dao = SingletonDAO.getSingletonDAO();
                 List<User> users = dao.user.getAll(
                         String.format("WHERE Login='%s' and Password='%s' LIMIT 0,1",
@@ -24,6 +24,7 @@ public class CmdLogin extends Action {
                                 user.getPassword()
                         ));
                 if (users.size() == 1) {
+                    //user ok. save to session
                     user=users.get(0);
                     HttpSession session=req.getSession();
                     session.setAttribute("user",user);
